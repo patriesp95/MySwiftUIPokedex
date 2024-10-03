f76f23fbd43cedfc8c0f67e62e16068c4f12ae69
Patricia Martinez Espert <patricia.martinez@edreamsodigeo.com>
2024-10-03 13:53:31 +0200

    Initial Commit

---
+++ MySwiftUIPokedex/MySwiftUIPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIPokedexApp.swift
+//  MySwiftUIPokedex
+//
+//  Created by patricia.martinez on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
